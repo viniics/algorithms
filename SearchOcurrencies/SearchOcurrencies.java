d7dfa8176e657fce7e935907be4a90422f142d62
Vinicius <viniipvp@gmail.com>
2025-02-03 23:27:06 -0300

    progress

--- SearchOcurrencies/SearchOcurrencies.java
+++ SearchOcurrencies/SearchOcurrencies.java
@@ -3,13 +3,38 @@ package SearchOcurrencies;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-class SearchOcurrencies{
+class SearchOcurrencies {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        Path fileName = Path.of("C:\\Users\\vinii\\OneDrive\\Área de Trabalho\\ATAL\\algorithms\\SearchOcurrencies\\fileToRead.txt");
-        String fileToString  = Files.readString(fileName);
+        Path fileName = Path.of(
+                "C:\\Users\\vinii\\OneDrive\\Área de Trabalho\\ATAL\\algorithms\\SearchOcurrencies\\fileToRead.txt");
+        String fileToString = Files.readString(fileName);
+        System.out.print("Faça sua pesquisa: ");
         String search = sc.nextLine();
+        sc.close();
+        System.out.println(processResults(searchOcurrencies(search, fileToString)));
+
+    }
+
+    private static ArrayList<Integer> searchOcurrencies(String search, String file) {
+        ArrayList<Integer> resultIndexes = new ArrayList<>();
+
+        for (int i = 0; i < (file.length() - search.length()); i++) {
+            int currentPos = 0;
+            while (currentPos < file.length() && search.charAt(currentPos) == file.charAt(i + currentPos)) {
+                currentPos++;
+            }
+            if (currentPos == search.length()) {
+                resultIndexes.add(i);
+            }
+        }
+        return resultIndexes;
+    }
+
+    private static String processResults(ArrayList<Integer> list) {
+        return "ok";
     }
 }
