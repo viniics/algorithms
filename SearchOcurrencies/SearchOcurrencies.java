127700e4e5555bffb639fa01bb233d0ced79760e
Vinicius <viniipvp@gmail.com>
2025-02-03 22:55:21 -0300

    v1

--- SearchOcurrencies/SearchOcurrencies.java
+++ SearchOcurrencies/SearchOcurrencies.java
@@ -1,4 +1,15 @@
 package SearchOcurrencies;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 class SearchOcurrencies{
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        Path fileName = Path.of("C:\\Users\\vinii\\OneDrive\\Área de Trabalho\\ATAL\\algorithms\\SearchOcurrencies\\fileToRead.txt");
+        String fileToString  = Files.readString(fileName);
+        String search = sc.nextLine();
+    }
 }
