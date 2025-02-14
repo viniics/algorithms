b11d9f410e7cb54af71f17a03410e3c68dc74c79
Vinicius <viniipvp@gmail.com>
2025-02-13 23:11:13 -0300

    Subset Sum Fix

--- Backtracking/subset-sum.py
+++ Backtracking/subset-sum.py
@@ -14,8 +14,6 @@ Consider the ‘last’ element to be a part of the subset.
 Now the new required sum = required sum – value of ‘last’ element.
 Don’t include the ‘last’ element in the subset. Then the new required sum = old required sum.
 
-Mathematically the recurrence relation will look like the following:
-isSubsetSum(arr, n, sum) = isSubsetSum(arr, n-1, sum) OR isSubsetSum(arr, n-1, sum – arr[n-1])
 Base Cases:
 isSubsetSum(arr, n, sum) = false, if sum > 0 and n = 0
 isSubsetSum(arr, n, sum) = true, if sum = 0
@@ -28,8 +26,7 @@ def isSubsetSum(arr, n, sum):
         return False
     if(arr[n-1]>sum):
         return isSubsetSum(arr,n-1,sum)
-    # Agora, segundo o algoritmo, vamos do ultimo index ate o primeiro de forma iterativa.
-    #  o ultimo index ao final.
+    # Agora, segundo o algoritmo, vamos do ultimo index ate o primeiro de forma recursiva.
     return isSubsetSum(arr,n-1,sum) or isSubsetSum(arr,n-1,sum - arr[n-1])
 
 arr = [3, 34, 4, 12, 5, 2]
