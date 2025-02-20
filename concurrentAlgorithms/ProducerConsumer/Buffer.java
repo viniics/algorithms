8e2fcd9984fcf53de28801653b60521e5aa76271
Vinicius <viniipvp@gmail.com>
2025-02-20 05:51:49 -0300

    no Mutex exclusion lock

--- concurrentAlgorithms/ProducerConsumer/Buffer.java
+++ concurrentAlgorithms/ProducerConsumer/Buffer.java
@@ -1,27 +1,54 @@
 package concurrentAlgorithms.ProducerConsumer;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Buffer {
     private final int[] buffer;
-    private int itemCount = 0;
-    private int in = 0;
-    private int out = 0;
+    private final Lock lock = new ReentrantLock();
+    final Condition notFull = lock.newCondition();
+    final Condition notEmpty = lock.newCondition();
+    int inPos, takePos, count = 0;
 
-
-    public Buffer(int size){
+    public Buffer(int size) {
         this.buffer = new int[size];
     }
 
-    public void put(int value){
-        buffer[in] = value;
-        in++;
-        if(in==buffer.length){
-            in=0;
-        }
-        itemCount++;
-    }
-    public int get(){
-        int value = buffer[out];
+    public void put(int value) throws InterruptedException {
+        try {
+            while (count == buffer.length) {
+                notFull.await();
+            }
+            buffer[inPos] = value;
+            inPos++;
+            if (inPos == buffer.length) {
+                inPos = 0;
+            }
+            count++;
+            notEmpty.signalAll();
+        } finally {
 
-        return value;
-    } 
+        }
+
+    }
+
+    public int get() throws InterruptedException {
+        try {
+            while (count == 0) {
+                notEmpty.await();
+            }
+            int value = buffer[takePos];
+            takePos++;
+            if (takePos == buffer.length) {
+                takePos = 0;
+            }
+            count--;
+            notFull.signalAll();
+            return value;
+        } finally {
+
+        }
+
+    }
 }
