225ba4343230f549c8cf415edf406e7a8bb6bebd
Vinicius <viniipvp@gmail.com>
2025-02-25 11:07:34 -0300

    working solution, comparable utilization, todo atividadeService

--- Greedy/miniteste4atal/Atividade.java
+++ Greedy/miniteste4atal/Atividade.java
@@ -1,14 +1,16 @@
 package Greedy.miniteste4atal;
 
-public class Atividade {
+public class Atividade implements Comparable<Atividade> {
     private int horaInicio;
     private int horaTermino;
     private String nome;
+    private int duracaoAtividade;
 
     public Atividade(int horaInicio, int horaTermino, String nome) {
         this.horaInicio = horaInicio;
         this.horaTermino = horaTermino;
         this.nome = nome;
+        this.duracaoAtividade = horaTermino - horaInicio;
     }
 
     public String getNome() {
@@ -33,7 +35,16 @@ public class Atividade {
 
     @Override
     public String toString() {
-        return "Atividade [nome=" + nome + "]";
+        return "Atividade " + nome;
+    }
+
+    public int getDuracaoAtividade() {
+        return duracaoAtividade;
+    }
+
+    @Override
+    public int compareTo(Atividade a) {
+        return this.duracaoAtividade - a.getDuracaoAtividade();
     }
 
 }
--- Greedy/miniteste4atal/Solucao.java
+++ Greedy/miniteste4atal/Solucao.java
@@ -2,20 +2,21 @@ package Greedy.miniteste4atal;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Solucao {
     Utils utils = new Utils();
-    SortByLowestExecutionTime sortByLowestExecutionTime = new SortByLowestExecutionTime();
-
+    //TODO: Levar todo esse codigo pra o Atividade service e limpar o main
     void main() {
-        Atividade a1 = new Atividade(10, 12,"a1");
-        Atividade a2 = new Atividade(14, 18,"a2");
-        Atividade a3 = new Atividade(18, 19,"a3");
-        List<Atividade> atividades = Arrays.asList(a3,a1, a2);
-        sortByLowestExecutionTime.sort(atividades);
+        Atividade a1 = new Atividade(10, 12, "a1");
+        Atividade a2 = new Atividade(14, 18, "a2");
+        Atividade a3 = new Atividade(18, 19, "a3");
+        Atividade a4 = new Atividade(8, 22, "a4");
+        List<Atividade> atividades = Arrays.asList(a1, a2, a3, a4);
+        Collections.sort(atividades);
         List<Atividade> greedyAtividades = greedyAtividades(atividades);
-        System.out.println(greedyAtividades.toString()); //todo: colocar melhor como as atividades sao representadas
+        System.out.println(greedyAtividades.toString());
     }
 
     List<Atividade> greedyAtividades(List<Atividade> atividades) {
@@ -36,7 +37,5 @@ public class Solucao {
         }
         return true;
     }
-
-    
 
 }
