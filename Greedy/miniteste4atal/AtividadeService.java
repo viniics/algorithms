e8f713b70feb93314dfc673aa8148fb0d69b8386
Vinicius <viniipvp@gmail.com>
2025-02-25 11:15:09 -0300

    final working solution :)

--- Greedy/miniteste4atal/AtividadeService.java
+++ Greedy/miniteste4atal/AtividadeService.java
@@ -1,5 +1,34 @@
 package Greedy.miniteste4atal;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class AtividadeService {
     //todo: organizar os metodos nessa classe e tirar do main
+    Utils utils = new Utils();
+
+    List<Atividade> greedyAtividades(List<Atividade> atividades) {
+        sort(atividades);
+        List<Atividade> greedyOut = new ArrayList<>();
+        for (Atividade atividade : atividades) {
+            if (horarioIsLivre(greedyOut, atividade)) {
+                greedyOut.add(atividade);
+            }
+        }
+        return greedyOut;
+    }
+
+    boolean horarioIsLivre(List<Atividade> currentAtividades, Atividade atividade) {
+        for (Atividade a : currentAtividades) {
+            if (utils.sobrepoeHorario(a, atividade)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void sort(List<Atividade> atividades){
+        Collections.sort(atividades);
+    }
 }
--- Greedy/miniteste4atal/Solucao.java
+++ Greedy/miniteste4atal/Solucao.java
@@ -1,41 +1,22 @@
 package Greedy.miniteste4atal;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Solucao {
-    Utils utils = new Utils();
-    //TODO: Levar todo esse codigo pra o Atividade service e limpar o main
+    private final AtividadeService atividadeService = new AtividadeService();
     void main() {
+        List<Atividade> atividades = criaAtividades();
+        System.out.println(atividadeService.greedyAtividades(atividades).toString());
+    }
+
+    List<Atividade> criaAtividades() {
         Atividade a1 = new Atividade(10, 12, "a1");
         Atividade a2 = new Atividade(14, 18, "a2");
         Atividade a3 = new Atividade(18, 19, "a3");
         Atividade a4 = new Atividade(8, 22, "a4");
-        List<Atividade> atividades = Arrays.asList(a1, a2, a3, a4);
-        Collections.sort(atividades);
-        List<Atividade> greedyAtividades = greedyAtividades(atividades);
-        System.out.println(greedyAtividades.toString());
+        Atividade a5 = new Atividade(5, 6, "a5");
+        List<Atividade> atividades = Arrays.asList(a1, a2, a3, a4, a5);
+        return atividades;
     }
-
-    List<Atividade> greedyAtividades(List<Atividade> atividades) {
-        List<Atividade> greedyOut = new ArrayList<>();
-        for (Atividade atividade : atividades) {
-            if (horarioIsLivre(greedyOut, atividade)) {
-                greedyOut.add(atividade);
-            }
-        }
-        return greedyOut;
-    }
-
-    boolean horarioIsLivre(List<Atividade> currentAtividades, Atividade atividade) {
-        for (Atividade a : currentAtividades) {
-            if (utils.sobrepoeHorario(a, atividade)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
 }
