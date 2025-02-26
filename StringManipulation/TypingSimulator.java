67b88aae69e8e01fff6901a69f82133d3c636504
Vinicius <viniipvp@gmail.com>
2025-02-26 15:48:06 -0300

    Random Speed Switching During Text

--- StringManipulation/TypingSimulator.java
+++ StringManipulation/TypingSimulator.java
@@ -32,7 +32,7 @@ class TypingSimulator {
         return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     }
 
-    // Metodo para simular uma digitação mais parecida com o das LLM's
+    // Metodo para simular uma digitação mais parecida com o das LLMs
     static int controlSpeed(int currentPos, int fraseLen) {
         // Nos primeiros 25% do texto, o terminal "lê" numa velocidade média
         if (currentPos < (fraseLen / 4)) {
