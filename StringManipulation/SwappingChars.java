eb2be10d76fa466ec91d6ff5cfed6896b47e5403
Vinicius <viniipvp@gmail.com>
2025-02-26 22:24:32 -0300

    Char Swap Java

--- StringManipulation/SwappingChars.java
+++ StringManipulation/SwappingChars.java
@@ -1,6 +1,16 @@
 // Uma vez, fiz uma entrevista de código onde a solução do problema envolvia a alteração de caracteres numa String.
 public class SwappingChars {
-    public static void main(String[] args) {
-        
+    void main(String[] args) {
+        String frase = "Nova frase";
+        frase = swap(frase, 2, 4);
+        System.out.println(frase);
+    }
+
+    String swap(String frase, int index1, int index2){
+        char[] temp = frase.toCharArray();
+        char swap = temp[index1];
+        temp[index1] = temp[index2];
+        temp[index2] = swap;
+        return new String(temp);
     }
 }
