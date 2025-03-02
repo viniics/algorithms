929a3f725676ae0fd14a7d21ec5b40259951af2a
Vinicius <viniipvp@gmail.com>
2025-03-02 08:18:27 -0300

    questao2570

---
+++ leetcode/Questao2570.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Questao2570 {
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        List<List<Integer>> finalList = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while (i<nums1.length || j<nums2.length) {
+            
+        }
+        return (int[][]) finalList.toArray();
+    }
+
+    private int act(int[][] arr1,int i){
+        return arr1[i][0];
+    }
+
+
+}
