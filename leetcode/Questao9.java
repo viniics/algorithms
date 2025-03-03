61a4bacf76325aeb4ea78a67fb6e26d6ef628874
Vinicius <viniipvp@gmail.com>
2025-03-03 07:41:18 -0300

    Questao9

---
+++ leetcode/Questao9.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class Questao9 {
+    // Primeiro, faremos transformando em String
+    public boolean isPalindrome(int x){
+        if(x<0){
+            return false;
+        }
+    
+    char[] intToCharArr = String.valueOf(x).toCharArray();
+    int i = 0;
+    int j = intToCharArr.length-1;
+    while (i!=j) {
+        if(intToCharArr[i]!=intToCharArr[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+    }
+}
