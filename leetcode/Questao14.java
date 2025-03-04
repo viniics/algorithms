302e1f1992817d45356923891432cdbcf6c7cbbf
Vinicius <viniipvp@gmail.com>
2025-03-04 07:29:43 -0300

    Questao 14

--- leetcode/Questao14.java
+++ leetcode/Questao14.java
@@ -3,10 +3,31 @@ package leetcode;
 
 public class Questao14 {
     public String longestCommonPrefix(String[] strs) {
-        
+        String prefix = "";
+        for (int i = 0; i < getMaxRange(strs); i++) {
+            char currentChar = strs[0].charAt(i);
+            for (String s : strs) {
+                if (s.charAt(i) != currentChar) {
+                    return prefix;
+                }
+            }
+            prefix += currentChar;
+        }
 
-
-
-        return "";
+        return prefix;
     }
+
+    int getMaxRange(String[] strs) {
+        if (strs.length == 0)
+            return 0;
+        int minLength = strs[0].length();
+        for (int i = 0; i < strs.length; i++) {
+            int currentLength = strs[i].length();
+            if (currentLength < minLength) {
+                minLength = currentLength;
+            }
+        }
+        return minLength;
+    }
+
 }
