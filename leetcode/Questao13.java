3d063455354d852bca43ba9d4da49a4d28ebdc6d
Vinicius <viniipvp@gmail.com>
2025-03-04 06:57:44 -0300

    Questao13

--- leetcode/Questao13.java
+++ leetcode/Questao13.java
@@ -8,4 +8,26 @@ public class Questao13 {
 
         return 0;
     }
+
+
+    public int convert(char c){
+        Character upper = Character.toUpperCase(c);
+        switch (upper) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+        }
+        return c;
+    }
 }
