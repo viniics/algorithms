5e59f241b706a07d9b1f1e6d50d383daf68e23ef
Vinicius <viniipvp@gmail.com>
2025-03-04 07:12:33 -0300

    Questao14

---
+++ leetcode/Questao14.java
@@ -0,0 +1,12 @@
+// Link: https://leetcode.com/problems/longest-common-prefix
+package leetcode;
+
+public class Questao14 {
+    public String longestCommonPrefix(String[] strs) {
+        
+
+
+
+        return "";
+    }
+}
