28cb4aafec92a0bb989a18f7192675a2168f4b0e
Vinicius <viniipvp@gmail.com>
2025-03-11 08:31:33 -0300

    Questao70

--- leetcode/Questao70.java
+++ leetcode/Questao70.java
@@ -1,13 +1,15 @@
 package leetcode;
 
 public class Questao70 {
-    public int climbStairs(int n){
-        if(n==0) return n;
-        if(n-2>=0) return 1 + climbStairs(n-2) + climbStairs(n-1);
-        else return 1 + climbStairs(n-1); 
+    public int climbStairs(int n) {
+        if (n == 0) return n;
+        if (n - 1 == 0) return 1;
+        if (n - 2 == 0) return 2;
+        if (n - 2 > 0) return climbStairs(n - 2) + climbStairs(n - 1);
+        else return climbStairs(n - 1);
     }
 
-    void main(){
-        System.out.println(climbStairs(3));
+    void main() {
+        System.out.println(climbStairs(45));
     }
 }
