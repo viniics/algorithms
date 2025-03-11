b831ff6197a7095df9ab3c91b6354c41c1092353
Vinicius <viniipvp@gmail.com>
2025-03-11 13:44:24 -0300

    Counting Sort

--- SortingAlgorithms/Other/CountingSort.java
+++ SortingAlgorithms/Other/CountingSort.java
@@ -6,18 +6,18 @@ public class CountingSort extends Algorithm {
     @Override
     public void sort(int[] array){
         int maxValue = utils.myMax(array);
-        int[] novoArray = new int[maxValue+1];
+        int[] auxArray = new int[maxValue+1];
         // Preenche o array auxiliar
         for (int i = 0; i < array.length;i++){
             int value = array[i];
-            novoArray[value] ++;
+            auxArray[value] ++;
         }
         
         int c = 0;
         
-        for(int i = 1;i<=novoArray.length;i++){
-            while (novoArray[i-1]>0) {
-                novoArray[i-1] --;
+        for(int i = 1;i<=auxArray.length;i++){
+            while (auxArray[i-1]>0) {
+                auxArray[i-1] --;
                 array[c] = i-1;
                 c++;
             }
