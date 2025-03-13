65f31bca7401de01e046f139043db09bbbcefad3
Vinicius <viniipvp@gmail.com>
2025-03-13 08:01:01 -0300

    MultiplicacaoDeMatrizes

--- DivideAndConquer/MultiplicacaoMatrizes.java
+++ DivideAndConquer/MultiplicacaoMatrizes.java
@@ -1,5 +1,14 @@
 package DivideAndConquer;
 
-public class MultiplicacaoMatrizes{
-    
+public class MultiplicacaoMatrizes {
+    int[][] matriz1 = { { 3, 7 }, { 1, 9 } };
+
+    int[][] matriz2 = { { 6, 2 }, { 8, 4 } };
+
+    int multiplicaMatriIterativo(int[][] matriz1, int[][] matriz2){
+        // Aqui, sera suposto que so serao multiplicadas matrizes de ordem axb * bxc
+
+
+        return 0;
+    }
 }
