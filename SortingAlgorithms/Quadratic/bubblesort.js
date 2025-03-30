c2f6e943f252256a7fd5e0c8f9201d3118e773b0
Vinicius <viniipvp@gmail.com>
2025-03-30 12:26:28 -0300

    javascript bubblesort

--- SortingAlgorithms/Quadratic/bubblesort.js
+++ SortingAlgorithms/Quadratic/bubblesort.js
@@ -1,6 +1,6 @@
 function bubblesort(array){
     for(i=0;i<array.length-1;i++){
-        for(j=i;j<array.length;j++){
+        for(j=i+1;j<array.length;j++){
             if(array[i]>array[j]){
                 let temp = array[i]
                 array[i]= array[j]
@@ -9,7 +9,7 @@ function bubblesort(array){
         }
     }
 }
-array = [1,2,4,5,6,7,8,9,2,4,5,6,7,0]
+array = [9,8,7,6,5,4,3,2,1,0]
 console.log(array + "-> array original")
 bubblesort(array)
 console.log(array + "-> array ordenado")
